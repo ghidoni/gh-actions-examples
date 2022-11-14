be9d35dcd0bef95e25e7005f3f13050514d4181b
ghidoni <thighidoni@gmail.com>
2022-11-13 23:21:02 -0300

    add dummy main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""Dummy Module"""
+
+
+def test_func(inputX):
+    """Dummy Functions
+
+    Args:
+        x (int): number input
+
+    Returns:
+        int: number input squared
+    """
+    return inputX**2
